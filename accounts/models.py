6a1164ac9671369e1f4c105aeb1d6bb838971023
Divyam Khanna <divyam.dkhanna@gmail.com>
2026-08-03 15:23:30 +0530

    INITIAL COMMIT

---
+++ accounts/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+
+        if not email:
+            raise ValueError("Email field is required")
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+
+        user.set_password(password)
+
+        user.save(using=self._db)
+
+        return user
+
+
+    def create_superuser(self, email, password=None, **extra_fields):
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        return self.create_user(
+            email,
+            password,
+            **extra_fields
+        )
+
+
+class User(AbstractUser):
+
+    username = models.CharField(
+        max_length=150,
+        unique=False,
+        blank=True,
+        null=True
+    )
+
+    email = models.EmailField(
+        unique=True
+    )
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
