2f1ef10e906fd8cc8cd9c23e81b388b34c55f8dd
Divyam Khanna <divyam.dkhanna@gmail.com>
2026-08-14 16:11:08 +0530

    Made the Right choice of default database.

--- personal_expense_tracker/settings.py
+++ personal_expense_tracker/settings.py
@@ -84,11 +84,7 @@ WSGI_APPLICATION = 'personal_expense_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'postgresql_neon':{
+    'default':{
         'ENGINE':'django.db.backends.postgresql',
         'NAME': env('postgresql_neon_NAME'),
         'USER': env('postgresql_neon_USER'),
